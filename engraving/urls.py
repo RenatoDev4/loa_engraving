f20582ee6ea6e291f55631a04642de6d6177de7c
Renato Moraes <renatodev4@gmail.com>
2023-05-30 11:54:55 -0300

    Remove Admin Area

--- engraving/urls.py
+++ engraving/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
 from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('duvidas/', include('duvidas.urls')),
     path('merchantbot/', include('merchantbot.urls')),
